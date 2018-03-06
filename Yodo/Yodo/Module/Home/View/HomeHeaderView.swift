fdbe899d89c2e76835f04dfb567792025f02e0de
lyimin <1142343535@qq.com>
2018-03-06 18:45:01 +0800

    feat: add HomeHeaderView

---
+++ Yodo/Yodo/Module/Home/View/HomeHeaderView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeHeaderView.swift
+//  Yodo
+//
+//  Created by 梁亦明 on 2018/3/6.
+//  Copyright © 2018年 com.eamon. All rights reserved.
+//
+
+import UIKit
+
+class HomeHeaderView: UIView {
+
+   
+    // Getter | Setter
+    
+    private lazy var menuBtn: UIButton = {
+        
+        var menuBtn = UIButton()
+        return menuBtn
+    }()
+    
+    private lazy var sepLine: UIView = {
+        
+        var sepLine = UIView()
+        return sepLine
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+       
+        var titleLabel = UILabel()
+        return titleLabel
+    }()
+}
