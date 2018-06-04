5352584c2bf66bbe5f5fe4a49fd62f615197ddf0
lyimin <1142343535@qq.com>
2018-06-04 23:43:48 +0800

    feat:add shake func

---
+++ Yod/Yod/Config/YodConst.swift
@@ -0,0 +1,21 @@
+//
+//  YodConst.swift
+//  Yod
+//
+//  Created by eamon on 2018/6/4.
+//  Copyright © 2018年 com.eamon. All rights reserved.
+//
+
+import UIKit
+
+class YodConst: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
