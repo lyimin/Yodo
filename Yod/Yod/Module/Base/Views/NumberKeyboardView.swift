b21810fede144ee1112cc88d66ba934d6ff30675
lyimin <1142343535@qq.com>
2018-06-07 00:51:03 +0800

    fix:调换键盘c和.位置

--- Yod/Yod/Module/Base/Views/NumberKeyboardView.swift
+++ Yod/Yod/Module/Base/Views/NumberKeyboardView.swift
@@ -103,7 +103,7 @@ class NumberKeyboardView: UIView {
     private var titleArray: [String] = ["1", "2", "3",
                                         "4", "5", "6",
                                         "7", "8", "9",
-                                        "c", "0", "."]
+                                        ".", "0", "c"]
     private var defaultText: String = "- 0.00"
 }
 
